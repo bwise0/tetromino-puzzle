6ba5c047fee936ee2201dde70703f77a604c9c86
sangchul jung <bwise0@gmail.com>
2026-03-24 17:32:55 +0900

    fix: github pages 404 경로 문제 해결을 위해 상대경로 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // GitHub 저장소 이름과 동일하게 base 경로를 설정해야 올바르게 접속됩니다.
-  base: '/tetromino-puzzle/',
+  // 절대경로 대신 상대경로 './' 로 변경하면 어떠한 이름의 깃허브 저장소에서든
+  // 에셋(assets)의 404 오류 추적을 완벽하게 자동으로 맞춰줍니다.
+  base: './',
 });
